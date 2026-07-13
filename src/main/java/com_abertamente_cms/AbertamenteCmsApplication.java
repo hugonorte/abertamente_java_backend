6a64657b1f3849d9839da8bb2ccc03a190639a67
Hugo Norte <hugonorte@gmail.com>
2026-07-13 11:26:55 -0300

    Initial commit

---
+++ src/main/java/com_abertamente_cms/AbertamenteCmsApplication.java
@@ -0,0 +1,13 @@
+package com_abertamente_cms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AbertamenteCmsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AbertamenteCmsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com_abertamente_cms/AbertamenteCmsApplicationTests.java
@@ -0,0 +1,13 @@
+package com_abertamente_cms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AbertamenteCmsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
